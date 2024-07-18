970afa8badc85fd495975b79d654261e3dffe3b8
Pongphat.Mit <pongphat.mit@gmail.com>
2024-07-18 13:22:41 +0700

    dwsd

--- Model/detect_carplate.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-import imutils 
-import easyocr
-
-img = cv2.imread(r"C:\Users\pongp\Downloads\Senior\DataSet\download.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
